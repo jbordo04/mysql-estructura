b5d50c282f712834998fd3afe55b68ba6a8b9910
shakebbyisok <nexlx99@gmail.com>
2024-02-07 13:59:21 +0100

    sql nivel 1

--- src/infrastructure/database/optica.sql
+++ src/infrastructure/database/optica.sql
@@ -1,12 +1,15 @@
--- drop DATABASE if exist 'sql-SP5'
--- create DATABASE 'sql-SP5'
--- use `sql-SP5`
+-- DROP DATABASE --
+drop DATABASE if exists optica;
+
+-- CREATE and USE this DATABASE --
+create DATABASE optica;
+use `optica`;
 
 -- set names utf8
 -- set character_set_client  =  utf8mb4
 
 
---DELETE TABLES --
+-- DELETE TABLES --
 drop table if exists ventas;
 drop table if exists montura;    
 drop table if exists customer;
@@ -14,27 +17,27 @@ drop table if exists gafas;
 drop table if exists proveidor; 
 drop table if exists employee; 
 
---CREATE TABLES --
+-- CREATE TABLES --
 CREATE TABLE proveidor  (
    id integer PRIMARY KEY,
    adress text NOT NULL,  
-   telephone integer NOT NULL,
+   telephone text NOT NULL,
    fax integer,
    NIF integer NOT NULL   
 );
 
 CREATE TABLE montura  (
-   id integer PRIMARY KEY,
+   id integer PRIMARY KEY AUTO_INCREMENT,
    name text NOT NULL
 );
 
 CREATE TABLE gafas  (
-   id integer PRIMARY KEY,
+   id integer PRIMARY KEY AUTO_INCREMENT,
    brand  text NOT NULL,
    graduation integer NOT NULL,
-   idMontura integer ,
+   idMontura integer,
    colourJoint text NOT NULL,
-   colorGlass text NOT NULL UNIQUE,
+   colorGlass text NOT NULL,
    idProveidor integer NOT NULL,
    price integer not null,
    foreign key (idProveidor) references proveidor (id),
@@ -42,24 +45,26 @@ CREATE TABLE gafas  (
 );
 
 CREATE TABLE customer  (
-   id integer PRIMARY KEY,
+   id integer PRIMARY KEY AUTO_INCREMENT,
    name  text NOT NULL,
    adress text NOT NULL,
-   telephone integer UNIQUE,
+   telephone text,
    email text NOT NULL,
    date date,
    idCliente_Recomended integer,
-   foreign key (idCliente_Recomended) references customer (id)
+   foreign key (idCliente_Recomended) references customer (id),
+   constraint UC_telephone UNIQUE (telephone)
 );
 
 CREATE TABLE employee (
-   id integer PRIMARY KEY,
+   id integer PRIMARY KEY AUTO_INCREMENT,
    nombre text NOT NULL,
    apellido text NOT NULL,
-   turno text NOT NULL) 
+   turno text NOT NULL
+);
 
 CREATE TABLE ventas (
-   id integer PRIMARY KEY,
+   id integer PRIMARY KEY AUTO_INCREMENT,
    idCustomer  integer NOT NULL,
    idEmployee int NOT NULL,
    idGafas integer NOT NULL,
@@ -67,4 +72,51 @@ CREATE TABLE ventas (
    foreign key (idCustomer) references customer (id),
    foreign key (idEmployee) references employee (id),
    foreign key (idGafas) references gafas (id)
+);
+
+INSERT INTO proveidor (adress, telephone, fax, NIF) values 
+(
+   'Ave.Meridiana 57', '6734522345', '4437654', 'X4352345T'
+),
+(
+   'Ave.Roma 234', '345634563', '4437654', 'F23423R'
+),
+(
+   'Ave.Gipuzcoia 23', '2345522345', '4437654', 'T234234T'
+);
+
+INSERT INTO montura (name) values ('flotant'),('pasta'),('metalica');
+
+INSERT INTO gafas ( brand, graduation, idMontura, colourJoint, colorGlass, idProveidor, price) values 
+(
+   'Rayban', 2.3, 1, 'amarillo', 'verde', 1, 60.5
+),
+(
+   'DG', 1, 2, 'verde', 'negro', 2, 80.9
+),
+(
+   'chichin', 4.5, 3, 'rojo', 'marron', 1, 39.9
+);
+
+
+INSERT INTO employee (name, apellido, turno) values 
+(
+   'joan', 'bordonaba', 'tarde'
+),
+(
+   'Mireia', 'Gonzales', 'maana'
+),
+(
+   'Floren', 'Straush', 'maana'
+);
+
+INSERT INTO customer (name, adress, telephone, email, date, idCliente_Recomended) values 
+(
+   'Roberto', 'Boreal 6 1-1', '680566146', 'joan@#sdfasdf.com', now(), null
+),
+(
+   'Maria', 'roma 7 4-2', '4563472342', 'maria@#sdfasdf.com', now(), null
+),
+(
+   'Ana', 'burgos 45 3-1', '6234561345', 'ana@#sdfasdf.com', now(), null
 );
---
+++ src/infrastructure/database/youtobe.sql
@@ -0,0 +1,94 @@
+-- DROP DATABASE --
+drop DATABASE if exists youtube;
+
+-- CREATE and USE this DATABASE --
+create DATABASE youtube;
+use `youtube`;
+
+-- set names utf8
+-- set character_set_client  =  utf8mb4
+
+-- CREATE TABLE --
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  email text not null,
+  password text not null,
+  nickname text not null,
+  birthdate datetime not null,
+  sex varchar(255) not null, 
+  country varchar(255) not null,
+  CP text not null
+);
+
+CREATE table typeVideo(
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  name varchar(255) not null
+);
+
+CREATE TABLE labels (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  name varchar(255) not null
+);
+
+CREATE TABLE videos (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  title text not null,
+  description text not null,
+  size integer not null,
+  name text not null,
+  duration integer not null,
+  thunmbnail text not null,
+  num_Plays integer not null,
+  num_Likes integer not null,
+  num_Dislikes integer not null,
+  idTypeVideo integer not null,
+  idUser integer not null,
+  date datetime not null,
+  idLabels int not null,
+  foreign key (idTypeVideo) references typeVideo(id),
+  foreign key (idUser) references users(id),
+  foreign key (idLabels) references labels(id)
+);
+
+CREATE table statePlaylist(
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  name varchar(255) not null
+);
+
+CREATE TABLE playlist(
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  date date not null,
+  idState INTEGER NOT NULL,
+  idVideo int,
+  foreign key (idVideo) references videos(id),
+  foreign key (idState) references statePlaylist(id)
+);
+
+CREATE TABLE coments (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  content text not null,
+  date datetime not null,
+  idUser integer not null,
+  idVideo integer not null,
+  foreign key (idUser) references users(id),
+  foreign key (idVideo) references videos(id)
+);
+
+CREATE TABLE likedVideo(
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  idVideo integer not null,
+  idUser integer not null,
+  date datetime not null,
+  foreign key (idVideo) references videos(id),
+  foreign key (idUser) references users(id)
+);
+
+CREATE TABLE nolikedVideo(
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  idVideo integer not null,
+  idUser integer not null,
+  date datetime not null,
+  foreign key (idVideo) references videos(id),
+  foreign key (idUser) references users(id)
+);
